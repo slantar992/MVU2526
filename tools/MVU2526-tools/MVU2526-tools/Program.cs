0298d5c5d9ea9545da3dbcd96021deeca393c657
Slantar <38003189+slantar992@users.noreply.github.com>
2026-05-07 17:45:01 +0200

    Run unity build on tools project

--- tools/MVU2526-tools/MVU2526-tools/Program.cs
+++ tools/MVU2526-tools/MVU2526-tools/Program.cs
@@ -1,3 +1,8 @@
 ﻿
 
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+
+var arguments = @"-projectPath ""C:\Users\UMA\Documents\GitHub\MVU2526\MVU2526"" -buildWindows64Player ""C:\Users\UMA\Documents\GitHub\MVU2526\MVU2526\Builds\Game.exe"" -quit";
+
+
+Process.Start(@"C:\Program Files\Unity\Hub\Editor\6000.3.13f1\Editor\unity.exe", arguments);

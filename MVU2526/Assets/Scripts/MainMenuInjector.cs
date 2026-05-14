10cc42566dff3b42f6e03c17b40c920a68567042
Slantar <38003189+slantar992@users.noreply.github.com>
2026-05-14 20:35:55 +0200

    Property change and command bindings

--- MVU2526/Assets/Scripts/MainMenuInjector.cs
+++ MVU2526/Assets/Scripts/MainMenuInjector.cs
@@ -1,17 +1,73 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using UnityEngine;
 
 public class MainMenuInjector : MonoBehaviour
 {
+    MainMenuViewModel viewModel;
+
     private void Start()
     {
         var noesis = GetComponent<NoesisView>();
 
-        noesis.Content.DataContext = new MainMenuViewModel();
+        viewModel = new MainMenuViewModel();
+
+        noesis.Content.DataContext = viewModel;
     }
 
 }
 
-public class MainMenuViewModel
+public class MainMenuViewModel : INotifyPropertyChanged
 {
-    public float Number => 4;
+    public float Number
+    {
+        get => number;
+        private set
+        {
+            number = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public DelegateCommand IncrementNumber { get; private set; }
+
+    private float number;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public MainMenuViewModel()
+    {
+        IncrementNumber = new DelegateCommand(() => Number++);
+    }
+
+    // Create the OnPropertyChanged method to raise the event
+    // The calling member's name will be used as the parameter.
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
+
+public class DelegateCommand : ICommand
+{
+    private Action onExecute;
+
+    public DelegateCommand(Action onExecute)
+    {
+        this.onExecute = onExecute;
+    }
+
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter)
+    {
+        return true;
+    }
+
+    public void Execute(object parameter)
+    {
+        onExecute?.Invoke();
+    }
 }
